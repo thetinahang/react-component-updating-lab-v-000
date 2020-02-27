aa8ea7f5454ab24d3eb750fafed2a08f5ba2d8e4
Tina Hang <mstinahang@gmail.com>
2020-02-27 02:11:42 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -15,6 +15,10 @@ class Timer extends Component {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
+  shouldComponentUpdate() {
+    
+  }
+
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
